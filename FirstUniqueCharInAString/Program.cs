77ea2d53c4b31f1bed4c67722bd358d16a1ba322
Tim <timothy.b.stott@gmail.com>
2022-08-16 10:42:39 -0400

    Add project files.

---
+++ FirstUniqueCharInAString/Program.cs
@@ -0,0 +1,49 @@
+﻿// Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+using System;
+
+namespace FirstUniqueCharInAString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstUniqChar("test")) ;
+        }
+
+
+        public static int FirstUniqChar(string s)
+        {
+            int answer = -1;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                bool duplicated = false;
+                int charCount = 0;
+
+
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (s[i] == s[j]) 
+                    { 
+                        charCount++; 
+                    }
+                    if ( charCount > 1) 
+                    {
+                        duplicated = true;
+                        break; 
+                    }                    
+                }
+
+
+                if (!duplicated)
+                {
+                    answer = i;
+                    break;
+                }                    
+            }
+
+            return answer;
+        }
+    }
+}
